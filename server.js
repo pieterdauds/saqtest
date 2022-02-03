27a0c8462240db2c189278ba541babf0d372dcc5
Pieter Daud Satumalay <peter.crusties@gmail.com>
2022-02-03 20:07:55 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello SAQ!\n'
+  const msg = 'Hello SAQ Test!\n'
   res.end(msg);
 });
 
